2ccb857596d6c194dfb39a91add1732978dfe35e
f4cu333 <f4cu.30@gmail.com>
2022-03-18 11:28:35 -0300

    .

---
+++ hola_mundo.py
@@ -0,0 +1,10 @@
+"hola mundo"
+
+print("hola mundo")
+print("esto es una prueba")
+print(10-1)
+
+numero1 = 10
+numero2 = 5
+resultado = numero1 * numero2
+print('El producto entre ' + str(numero1) + ' y ' + str(numero2) + ' da ' + str(resultado))
