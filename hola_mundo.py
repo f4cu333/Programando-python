6a76d17bcf28ac6ad7eabb3f97271d626194420b
f4cu333 <f4cu.30@gmail.com>
2022-03-18 11:41:57 -0300

    .

--- hola_mundo.py
+++ hola_mundo.py
@@ -8,3 +8,13 @@ numero1 = 10
 numero2 = 5
 resultado = numero1 * numero2
 print('El producto entre ' + str(numero1) + ' y ' + str(numero2) + ' da ' + str(resultado))
+miNombre = "Facundo Ghisoni"
+miNombre = 'Facundo'
+miApellido = 'Ghisoni'
+miEdad = '21'
+
+print('Mi nombre completo es ' + miNombre + " " + miApellido + " y tengo " + miEdad)
+miNumeroFavorito = 10
+
+print("Mi numero favorito es: " + str(miNumeroFavorito))
+
